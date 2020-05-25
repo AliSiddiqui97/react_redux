8f3af29b6d534ec79942ce0ac46d7ee886cb7e87
AliSiddiqui97 <alisiddiqui1997@hotmail.com>
2020-05-25 05:05:10 +0500

    tutorial

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+export const increment =(numb)=>{
+    return{
+        type:"INCREMENT",
+        payload: numb
+    }
+}
+export const decrement =()=>{
+    return{
+        type:"DECREMENT"
+    }
+}
